6954eaaadf6b64e487feb43e76f118655f6b6cfc
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-03-30 14:07:30 +0900

    Java_Comments

---
+++ src/package2/Java_Comments.java
@@ -0,0 +1,17 @@
+package package2;
+
+public class Java_Comments {
+    public static void main(String[] args) {
+        // comment
+
+        // Example 1 : Java Single Line Comment
+
+        /*
+        Example 2 : Java multiline Comment
+         */
+
+        /**
+         * Example 3 : Documentaion Comment
+         */
+    }
+}
