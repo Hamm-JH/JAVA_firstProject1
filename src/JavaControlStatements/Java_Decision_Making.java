ba19a4309142b26463d3df9cf3316af8ddad4b41
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-03-30 14:27:50 +0900

    Java_Decision_Making

---
+++ src/JavaControlStatements/Java_Decision_Making.java
@@ -0,0 +1,13 @@
+package JavaControlStatements;
+
+public class Java_Decision_Making {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = (a == 1) ? 20: 30;
+        System.out.println("Value of b is : " + b);
+
+        b = (a == 10) ? 20: 30;
+        System.out.println("Value of b is : " + b);
+    }
+}
