769795102721fe45e72ef06cda1736fa61733f2a
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-03-30 11:06:45 +0900

    Java_Variable Types

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,17 @@
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//
+//        // Example of Valid Variables Decalrations and Initializations
+////        int a, b, c;            // Declares three ints, a, b, and c.
+////        int a = 10, b = 10;     // Example of initialization
+////        byte B = 22;            // initializes a byte type variable B.
+////        double pi = 3.14159;    // declares and assigns a value of PI.
+////        char a = 'a';           // the char variable a iis initialized with value
+//
+//
+//
+//    }
 }
