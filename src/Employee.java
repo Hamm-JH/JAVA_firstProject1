bc4632ad0ce274f6b5cf1e4aaa3ab72f1afffffb
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-03-30 12:29:50 +0900

    Java_Data Types

--- src/Employee.java
+++ src/Employee.java
@@ -5,8 +5,8 @@ public class Employee {
 
     public static final String DEPARTMENT = "Development ";
 
-    public static void main(String args[]) {
-        salary = 1000;
-        System.out.println(DEPARTMENT + "average salary:" + salary);
-    }
+//    public static void main(String args[]) {
+//        salary = 1000;
+//        System.out.println(DEPARTMENT + "average salary:" + salary);
+//    }
 }
---
+++ src/package2/JavaTester.java
@@ -0,0 +1,29 @@
+package package2;
+
+public class JavaTester {
+    public static void main(String args[]) {
+        byte byteValue1 = 2;
+        byte byteValue2 = 4;
+        byte byteResult = (byte)(byteValue1 + byteValue2);
+
+        System.out.println("Byte: " + byteResult);
+
+        short shortValue1 = 2;
+        short shortValue2 = 4;
+        short shortResult = (short)(shortValue1 + shortValue2);
+
+        System.out.println("Short: " + shortResult);
+
+        int intValue1 = 2;
+        int intValue2 = 4;
+        int intResult = intValue1 + intValue2;
+
+        System.out.println("Int: " + intResult);
+
+        long longValue1 = 2L;
+        long longValue2 = 4L;
+        long longResult = longValue1 + longValue2;
+
+        System.out.println("Long: " + longResult);
+    }
+}
